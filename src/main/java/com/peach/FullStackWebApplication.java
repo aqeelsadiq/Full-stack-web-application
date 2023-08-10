d769fec9e5ead3fae0fc2ebd802101de1f252503
Tao, Wei <wtao31@gatech.edu>
2023-08-10 12:46:42 -0400

    Renamed for good practice

--- src/main/java/com/peach/FullStackWebApplication.java
+++ src/main/java/com/peach/FullStackWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Phase3TryApplication {
+public class FullStackWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Phase3TryApplication.class, args);
+        SpringApplication.run(FullStackWebApplication.class, args);
     }
 
 }
